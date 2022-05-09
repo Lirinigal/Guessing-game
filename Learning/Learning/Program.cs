63ba6475637590b40b713810bdd8422de202a4d9
Lirinigal <105148281+Lirinigal@users.noreply.github.com>
2022-05-09 17:56:02 +0300

    Add files via upload

---
+++ Learning/Learning/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Liri";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (secretWord != guess && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter Word: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+               
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.Write("You Win!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
